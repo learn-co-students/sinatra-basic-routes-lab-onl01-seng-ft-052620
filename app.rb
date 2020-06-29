22af4918ba2d7434d05d7dbed58fc7cbef0bc75b
Luca <lucakanyo@protonmail.com>
2020-06-28 20:33:00 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do 
+        "Hello, World!"
+    end 
+
+    get '/name' do 
+        "My name is Luca"
+    end 
+
+    get '/hometown' do 
+        "My hometown is Budapest"
+    end 
+
+    get '/favorite-song' do 
+        "My favorite song is changes"
+    end 
 end
