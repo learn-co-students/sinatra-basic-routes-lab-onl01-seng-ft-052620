6e53f2b4c1cf95fd9832f8109f985c7ceac62401
jonbleibdrey <jonbleibdrey@aol.com>
2020-07-01 17:58:53 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is jonathan bleibdrey"
+    end
+
+    get '/hometown' do
+        "My hometown is sharyland,texas"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is blink 182 - dammit"
+    end
+
 end
