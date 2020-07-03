c998c83236bf970919b651c8510a80df2ddbd227
lovecosma <williamsmatthew333@gmail.com>
2020-07-03 14:40:08 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Matthew"
+  end
+
+  get '/hometown' do
+    "My hometown is ???"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Siberian Breaks by MGMT"
+  end
+
 end
