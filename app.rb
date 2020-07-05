920aa070e213e26f70f25eac16b2172b417d6339
Eyeruz <3yerus@gmail.com>
2020-07-05 02:41:46 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/name' do 
+
+"My name is Eyerus K."
+end
+
+get '/hometown' do 
+
+"My hometown is Atl"
+end
+
+get '/favorite-song' do
+
+"My favorite song is Honest enough"
+
+end
+
 end
