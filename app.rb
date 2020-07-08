667c5623102186de04207163eae65ffe90549055
kkingston <kkingston09@gmail.com>
2020-07-08 15:18:13 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get "/" do
+        "Hello World!"
+    end
+
+    get "/name" do
+        "My name is Kayla!"
+    end
+
+    get "/hometown" do
+        "My hometown is Seattle."
+    end
+
+    get "/favorite-song" do
+        "My favorite song is Happy and You Know It."
+    end
 end
